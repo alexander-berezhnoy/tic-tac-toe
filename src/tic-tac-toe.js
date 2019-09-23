20ea7773ad456e8d46ffdfa09c05aee9ed5877e6
alexander-berezhnoy <berrezhnoy@gmail.com>
2019-09-23 18:37:10 +0300

    fix: solve task

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,35 +1,60 @@
 class TicTacToe {
-    constructor() {
+  constructor() {
+    this.currentPlayer = 'x';
+    this.field = [[null, null, null], [null, null, null], [null, null, null]];
+    this.turnCounter = 0;
+    // For 3 * 3 board, stateOfTheGame  = [row1, row2, row3, col1, col2, col3, diag1, diag2]
+    this.stateOfTheGame = new Array(8).fill(0);
+  }
 
+  getCurrentPlayerSymbol() {
+    return this.currentPlayer;
+  }
+
+  nextTurn(rowIndex, columnIndex) {
+    if (this.getFieldValue(rowIndex, columnIndex) === null) {
+      let point;
+      this.field[rowIndex][columnIndex] = this.getCurrentPlayerSymbol();
+      if (this.currentPlayer === 'x') {
+        this.currentPlayer = 'o';
+        point = 1;
+      } else {
+        this.currentPlayer = 'x';
+        point = -1;
+      }
+
+      this.turnCounter++;
+
+      this.stateOfTheGame[rowIndex] += point;
+      this.stateOfTheGame[columnIndex + 3] += point;
+      if (rowIndex === columnIndex) this.stateOfTheGame[6] += point;
+      if (rowIndex === 2 - columnIndex) this.stateOfTheGame[7] += point;
     }
+  }
 
-    getCurrentPlayerSymbol() {
+  isFinished() {
+    return !!this.getWinner() || this.isDraw();
+  }
 
-    }
+  getWinner() {
+    if (this.stateOfTheGame.includes(3)) return 'x';
+    if (this.stateOfTheGame.includes(-3)) return 'o';
+    return null;
+  }
 
-    nextTurn(rowIndex, columnIndex) {
+  noMoreTurns() {
+    return this.turnCounter === 9;
+  }
 
-    }
+  isDraw() {
+    return this.noMoreTurns() && !this.getWinner();
+  }
 
-    isFinished() {
-
-    }
-
-    getWinner() {
-
-    }
-
-    noMoreTurns() {
-
-    }
-
-    isDraw() {
-
-    }
-
-    getFieldValue(rowIndex, colIndex) {
-
-    }
+  getFieldValue(rowIndex, colIndex) {
+    return this.field[rowIndex][colIndex]
+      ? this.field[rowIndex][colIndex]
+      : null;
+  }
 }
 
 module.exports = TicTacToe;
